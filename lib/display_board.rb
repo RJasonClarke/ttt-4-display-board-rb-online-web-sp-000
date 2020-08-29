96dd878d7eef5ff63ebda641876ee0cc639748d9
Robert Clarke <r.jason.clarke@gmail.com>
2020-08-29 07:01:29 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -8,6 +8,8 @@ puts "-----------"
 puts " #{board[6]} | #{board[7]} | #{board[8]} "
 
 
+display_board (board)
+
   board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
 
 puts " #{board[0]} | #{board[1]} | #{board[2]} "
@@ -15,4 +17,3 @@ puts "-----------"
 puts " #{board[3]} | #{board[4]} | #{board[5]} "
 puts "-----------"
 puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
